5bf269c4411830bed2597ab566eba138517ac6a6
Ubuntu <ubuntu@ip-10-228-25-52.eu-west-1.compute.internal>
2012-09-09 19:45:59 +0000

    Initial rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Caloriestrack::Application.config.secret_token = 'da7dd22352f98025fd24ab768a112813a9a4c32bf2c9f95e1c73bc9e92fb130e9752b31bdd46c4380734e2d64edda1fab05622a36a8f16c0757a63eec591c23d'
